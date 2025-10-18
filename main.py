5e5b868994a5cba8e0029e1606ae5aa9ce5db40d
UnaiOlaiz <unai.olaizola.osa@opendeusto.es>
2025-10-18 11:06:06 +0200

    Script almost finished, now it's training time

--- main.py
+++ main.py
@@ -1,64 +1,150 @@
-# Libraries we will use
+# Libraries and dependencies we will use for this assignment
 import gymnasium as gym
 from stable_baselines3.common.callbacks import CheckpointCallback
 import ale_py
-from stable_baselines3 import SAC, PPO # SAC algorithm does not work for this environment
 from deustorl.common import evaluate_policy, EpsilonGreedyPolicy, max_policy
 from deustorl.sarsa import Sarsa
 from deustorl.qlearning import QLearning
 from deustorl.expected_sarsa import ExpectedSarsa
 import time
 import os
+from gymnasium import ObservationWrapper
+from gymnasium.spaces import Box
+import cv2
+import numpy as np
+from deustorl.helpers import DiscretizedObservationWrapper
+import optuna
+from optuna.samplers import TPESampler
+from optuna.pruners import MedianPruner
+import random
+import json
+from torch.utils.tensorboard import SummaryWriter
 
-os.system("rm -rf ./tensorboard_logs/")
-os.system("rm -rf ./checkpoints/")
-os.system("rm -rf ./models/")
-os.makedirs("./tensorboard_logs/", exist_ok=True)
-os.makedirs("./checkpoints/", exist_ok=True)
-os.makedirs("./models/", exist_ok=True)
+# We will use these classes to reduce the complexity of the environment, as it would not be feasible to execute it otherways (too many calculations)
+class DownsampleObservationWrapper(ObservationWrapper):
+    def __init__(self, env, new_shape=(8, 8)):
+        super().__init__(env)
+        self.new_shape = new_shape
+        self.observation_space = Box(low=0, high=255, shape=new_shape, dtype=np.uint8)
 
-gym.register_envs(ale_py)
-env_name = 'ALE/Boxing-v5'
-n_steps = 200_000
+    def observation(self, obs):
+        return cv2.resize(obs, self.new_shape, interpolation=cv2.INTER_AREA)
 
-env = gym.make(env_name, obs_type='grayscale', render_mode='rgb_array', frameskip=1, repeat_action_probability=0.25)
+class RamFeatureWrapper(ObservationWrapper):
+    def __init__(self, env, num_features=8):
+        super().__init__(env)
+        self.num_features = num_features
+        self.observation_space = Box(low=0, high=255, shape=(num_features,), dtype=np.uint8)
 
-# List of algorithms we will use
-algoritms = ['SARSA', 'Qlearning', 'Expectedsarsa']
+    def observation(self, obs):
+        return obs[:self.num_features]
 
-# Function we will use to solve the last 3 algorithms
-def test(algo, n_steps= 60000, **kwargs):
-    epsilon_greedy_policy = EpsilonGreedyPolicy(epsilon=0.1)
-    start_time = time.time()
-    algo.learn(epsilon_greedy_policy, n_steps, **kwargs)
-    print("----- {:0.4f} secs. -----".format(time.time() - start_time))
+# This function will try different hyperparameter configurations
+def hyperparameter_analysis(t):
+    # We first create the tensorboard log directory for this trial
+    tensorboard_log_dir = f"./tensorboard_logs/trial_{t.number}/"
+    os.makedirs(tensorboard_log_dir, exist_ok=True)
+    writer = SummaryWriter(log_dir=tensorboard_log_dir)
+    # Then define the hyperparameters to tune
+    algo_index = t.number % 3  # To cycle through the three algorithms
+    algorithm_name = ["Sarsa", "Qlearning", "Expectedsarsa"][algo_index]
+    learning_rate = t.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
+    discount_factor = t.suggest_float("discount_factor", 0.8, 1.0, step=0.025)
+    epsilon = t.suggest_float("epsilon", 0.0, 0.5, step=0.05)
+    lr_decay = t.suggest_float("lr_decay", 0.85, 1.0, step=0.01)
+    lr_episodes_decay = t.suggest_categorical("lr_episodes_decay",[100, 500, 1_000, 5_000, 10_000])
 
-    return evaluate_policy(algo.env, algo.q_table, max_policy, n_episodes=100, verbose=False)
+    # Number of steps to train
+    n_steps = 100_000
 
-for algo in algoritms:
-    if algo == "SARSA":
+    # Depending on the algorithm, we create the corresponding agent
+    if algorithm_name == "Sarsa":
+        print("Using SARSA algorithm")
         algo = Sarsa(env)
-        test(algo, n_steps=n_steps, lr=0.01)
-        checkpoint_dir = CheckpointCallback(save_freq=20_000, save_path=f"./checkpoints/checkpoint_{algo.lower()}/")
-        evaluate_policy(env, algo.q_table, max_policy, n_episodes=10, verbose=False) 
-    elif algo == "Qlearning":   
+        print("Trial number:", t.number)
+    elif algorithm_name == "Qlearning":
+        print("Using Q-learning algorithm")
+        print("Trial number:", t.number)
         algo = QLearning(env)
-        test(algo, n_steps=n_steps, lr=0.01)
-        checkpoint_dir = CheckpointCallback(save_freq=20_000, save_path=f"./checkpoints/checkpoint_{algo.lower()}/")
-        evaluate_policy(env, algo.q_table, max_policy, n_episodes=10, verbose=False)
-    elif algo == "Expectedsarsa":
+    elif algorithm_name == "Expectedsarsa":
+        print("Using Expected SARSA algorithm")
+        print("Trial number:", t.number)
         algo = ExpectedSarsa(env)
-        test(algo, n_steps=n_steps, lr=0.01)
-        checkpoint_dir = CheckpointCallback(save_freq=20_000, save_path=f"./checkpoints/checkpoint_{algo.lower()}/")
-        evaluate_policy(env, algo.q_table, max_policy, n_episodes=10, verbose=False)
 
-env.close()
+    # We will then set the policy we will use for training, in this case epsilon-greedy (with different values)
+    epsilon_greedy_policy = EpsilonGreedyPolicy(epsilon=epsilon)
+    algo.learn(epsilon_greedy_policy,n_steps=n_steps, discount_rate=discount_factor, lr=learning_rate, lrdecay=lr_decay, n_episodes_decay=lr_episodes_decay, verbose=False)
 
-# Creo que no funciona el SAC para este environment
-'''
-    elif algo == "SAC":
-        model = SAC('MlpPolicy', env, verbose=1, tensorboard_log=f"./tensorboard_logs/tensorboard_{algo.lower()}/")
-        model.learn(n_steps)
-        checkpoint_dir = CheckpointCallback(save_freq=20_000, save_path=f"./checkpoints/checkpoint_{algo.lower()}/")
-        model.save(f"./models/boxing_model_{algo.lower()}")
-    '''
+    avg_reward, avg_steps = evaluate_policy(algo.env, algo.q_table, max_policy, n_episodes=100)
+    writer.add_scalar("eval/avg_reward", avg_reward, t.number)
+    writer.add_scalar("eval/avg_steps", avg_steps, t.number)
+    # We will write the hyperparameters to then analyze them in Tensorboard
+    writer.add_hparams({
+        "algorithm": algorithm_name,
+        "learning_rate": learning_rate,
+        "discount_factor": discount_factor,
+        "epsilon":epsilon,
+        "lr_decay": lr_decay,
+        "lr_episodes_decay": lr_episodes_decay
+    }, {"avg_reward": avg_reward})
+    writer.close()
+    return avg_reward
+
+if __name__ == "__main__": 
+    # Removal of old logs and directories
+    os.system("rm -rf ./tensorboard_logs/")
+    os.system("rm -rf ./boxing_db/")
+    os.system("rm -rf ./boxing_optuna.db")
+    os.system("rm -rf ./Reinforcement-Learning/tensorboard_logs/")
+    os.system("rm -rf ./boxing_optuna/")
+    os.system("rm -rf ./boxing_logs/")
+    os.system("mkdir -p ./boxing_optuna/")
+    # Environment setup
+    environment_name = 'ALE/Boxing-v5'
+    env = gym.make(environment_name, obs_type='ram', render_mode='rgb_array', frameskip=1)
+    env = RamFeatureWrapper(env, num_features=8)
+    env = DiscretizedObservationWrapper(env, n_bins=4)
+
+    # Random seed
+    seed = 88
+    random.seed(seed)
+
+    # Optuna study setup
+    boxing_db = f"sqlite:///boxing_optuna/boxing_optuna.db"
+    environment_study_name = "boxing"
+    full_study_dir_path = f"boxing_optuna/{environment_study_name}"
+    sampler = TPESampler(seed=seed)
+    study = optuna.create_study(sampler=sampler, direction='maximize', study_name=environment_study_name, storage=boxing_db, load_if_exists=True)
+    
+    # Number of trials for the hyperparameter search
+    trials_per_alg = 50
+    total_trials = 3 * trials_per_alg  # We will do 150 trials in total
+
+    # Optuna hyperparameter search time
+    print(f"Starting the optuna hyperparameter search for environment: {environment_name}")
+    study.optimize(hyperparameter_analysis, n_trials=total_trials)
+
+    # We close the environment
+    env.close()
+
+    # Save the best trial hyperparameters
+    best_search = study.best_trial
+    best_search_params = json.dumps(best_search.params, sort_keys=True, indent=4)
+
+    # We create the study directory
+    os.system(f"mkdir -p {full_study_dir_path}")
+
+    # We keep the best trial parameters in a json file
+    best_trial_file = open(f"{full_study_dir_path}/best_trial.json", "w")
+    best_trial_file.write(best_search_params)
+    best_trial_file.close()
+
+    # Generate the important figures of the results
+    fig = optuna.visualization.plot_optimization_history(study)
+    fig.write_html(f"{full_study_dir_path}/optimization_history.html")
+    fig = optuna.visualization.plot_contour(study)
+    fig.write_html(f"{full_study_dir_path}/contour.html")
+    fig = optuna.visualization.plot_slice(study)
+    fig.write_html(f"{full_study_dir_path}/slice.html")
+    fig = optuna.visualization.plot_param_importances(study)
+    fig.write_html(f"{full_study_dir_path}/param_importances.html")
